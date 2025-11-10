0ade7c6c24260adf911fa0b29d54bde5bbb0498c
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-10 15:22:11 +0100

    created model class for books

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+namespace BookQuoteAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+    }
+}
