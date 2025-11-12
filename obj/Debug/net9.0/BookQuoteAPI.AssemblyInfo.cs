d2e5ffac5feac4d252741ab9efe109cc06b2d8be
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-12 23:04:09 +0100

    added file for users

--- obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69b76c35ff0c79e1b62b1860235aaed84a17a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472fd2324e3bb067df05d7c6aaece61ba644f209")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
