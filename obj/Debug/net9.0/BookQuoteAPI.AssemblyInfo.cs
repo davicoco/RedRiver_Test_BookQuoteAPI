a238a2b35521f85e208518469ebd2c019ead7ae5
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 12:05:40 +0100

    implemented controller class and added settings for jwt

--- obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285a38694eaedce8611b53790bc8357f0102a23b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5849214a4afd6249ed67dd97d43aafbecb0f17")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
