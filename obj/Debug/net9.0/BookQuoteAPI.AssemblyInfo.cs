685660f748126f19811b21fae017a024a1bc3dbc
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-15 15:25:08 +0100

    deleted auth controller

--- obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookQuoteAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca76158dff2f4d60e8e203eb3f39ef7116826fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1243b1a0f03fc0312da28f883bf8ed78216ff456")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookQuoteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
